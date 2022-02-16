4b51579793d566b933a20de2602ae81a5d24a792
Marcelo Marcos Barbosa <mmarcosbarbosa@gmail.com>
2022-02-16 15:27:46 -0300

    Curso de TypeScript em 16/02/2022

---
+++ typescript-curso-1/app/models/negociacao.ts
@@ -0,0 +1,15 @@
+export class Negociacao {
+    private _data;
+    private _quantidade;
+    private _valor;
+
+    constructor (data, quantidade, valor){
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+    get data() { return this._data; }
+    get quantidade() { return this._quantidade; }
+    get valor() { return this._valor; }
+    get volume() { return this._valor * this._quantidade }
+}
